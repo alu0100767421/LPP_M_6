7a78dd12f4e2e3955b46623d40b93bf714d315bb
Alexander Cole Mora <alu0100767421@ull.edu.es>
2014-10-16 17:46:53 +0100

    Resta, Multiplicación y Producto Escalar

--- lib/complejo.rb
+++ lib/complejo.rb
@@ -1,7 +1,19 @@
 def class complejo
 
-	def + (x,y)
-	
+	def - (n_complex)
+		Complejo.new(@real-n_complex.real, @imaginaria-n_complex.imaginaria)
 	end
 
+
+
+
+	def * (n_complex)
+		Complejo.new(@real*n_complex.real-@imaginaria*n_complex.imaginaria, @real*n_complex.imaginaria+@imaginaria*n_complex.real)
+
+	end
+
+	def prod_escalar (escalar)
+	
+		Complejo.new(escalar*@real, escalar*@imaginaria)
+	end
 end
